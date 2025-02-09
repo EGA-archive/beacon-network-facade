8a316350d8006b51ebb166f073193d6da23a8fdb
Giulia <giulia.cellerino@icloud.com>
2025-02-09 17:47:03 +0100

    Updated table rendering

--- client/src/BeaconQuery.js
+++ client/src/BeaconQuery.js
@@ -1,92 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import CollapsibleTable from "./Table";
-
-// function BeaconQuery({
-//   beaconId,
-//   beaconName,
-//   variant,
-//   genome,
-//   socket,
-//   registries,
-// }) {
-//   const [data, setData] = useState(null);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     console.log(`🚀 BeaconQuery Mounted for ${beaconName}`);
-//     console.log(`🔎 Variant: ${variant}, Genome: ${genome}`);
-//     console.log(`📡 Socket:`, socket);
-//     if (!beaconId || !variant || !genome || !socket) return;
-//     const arr = variant.split("-");
-//     if (arr.length !== 4) {
-//       setError("Invalid variant format");
-//       return;
-//     }
-
-//     const query = `/g_variants?start=${arr[1]}&alternateBases=${arr[3]}&referenceBases=${arr[2]}&referenceName=${arr[0]}&assemblyId=${genome}`;
-
-//     console.log(`📤 Sending Query to WebSocket:`, query);
-
-//     if (socket.readyState === WebSocket.OPEN) {
-//       socket.send(JSON.stringify(query));
-//     } else {
-//       setError("WebSocket not connected");
-//     }
-
-//     const handleMessage = (event) => {
-//       console.log(
-//         `📩 Received WebSocket Message for ${beaconName}:`,
-//         event.data
-//       );
-//       try {
-//         const response = JSON.parse(event.data);
-//         console.log(`✅ WebSocket JSON Response:`, response);
-//         setData(response);
-//       } catch (err) {
-//         console.error(`❌ Error parsing WebSocket message:`, err);
-//         setError("Invalid WebSocket response");
-//       }
-//     };
-//     socket.addEventListener("message", handleMessage);
-
-//     return () => {
-//       if (socket) socket.removeEventListener("message", handleMessage);
-//     };
-//   }, [beaconId, variant, genome, socket]);
-
-//   return (
-//     // <div>
-//     //   <h4>
-//     //     Beacon Query Result -{" "}
-//     //     <span style={{ color: "green" }}>{beaconName}</span>
-//     //     <br />
-//     //     <span style={{ color: "blue" }}>{beaconId}</span>
-//     //   </h4>
-//     //   {error && <p style={{ color: "red" }}>Error: {error}</p>}
-//     //   {data ? (
-//     //     <pre>{JSON.stringify(data, null, 2)}</pre>
-//     //   ) : (
-//     //     <p>Waiting for response...</p>
-//     //   )}
-//     // </div>
-//     <div>
-//       <h4>
-//         Beacon Query - <span style={{ color: "green" }}>{beaconName}</span> (
-//         <span style={{ color: "blue" }}>{beaconId}</span>)
-//       </h4>
-//       {error && <p style={{ color: "red" }}>Error: {error}</p>}
-//       {console.log("📊 Rendered Data:", data)}
-//       {data ? (
-//         <CollapsibleTable data={data} registries={registries} />
-//       ) : (
-//         <p>Waiting for response...</p>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default BeaconQuery;
-
 import React, { useEffect, useState } from "react";
 import CollapsibleTable from "./Table";
 
@@ -107,7 +18,13 @@ function BeaconQuery({
     console.log(`🔎 Variant: ${variant}, Genome: ${genome}`);
     console.log(`📡 Socket:`, socket);
 
-    if (!beaconId || !variant || !genome || !socket) return;
+    if (
+      //!beaconId ||
+      !variant ||
+      !genome ||
+      !socket
+    )
+      return;
     const arr = variant.split("-");
     if (arr.length !== 4) {
       setError("Invalid variant format");
@@ -160,10 +77,16 @@ function BeaconQuery({
 
   return (
     <div>
-      <h4>
-        Beacon Query - <span style={{ color: "green" }}>{beaconName}</span> (
-        <span style={{ color: "blue" }}>{beaconId}</span>)
-      </h4>
+      {registries.map((registry, index) => {
+        return (
+          <h4>
+            Beacon Query -{" "}
+            <span style={{ color: "green" }}>{registry.beaconName}</span> (
+            <span style={{ color: "blue" }}>{registry.beaconId}</span>)
+          </h4>
+        );
+      })}
+
       {error && <p style={{ color: "red" }}>Error: {error}</p>}
       {/* ✅ Pass aggregatedData instead of data */}
       {aggregatedData.length > 0 ? (
